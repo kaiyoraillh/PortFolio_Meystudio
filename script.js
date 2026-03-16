c56fbd79d4d6666067de44580812a357325ad3ed
Kai <kaiyoraillh2008@gmail.com>
2026-03-15 23:02:44 -0300

    Actualización desde Claude Code

--- script.js
+++ script.js
@@ -154,7 +154,7 @@ function createArtworkItem(artwork, index) {
       ${infoSection}
 
       <!-- LADO DERECHO: imagen clickeable -->
-      <div class="artwork-visual" data-title="${artwork.name}">
+      <div class="artwork-visual">
         <img
           src="${artwork.image}"
           alt="${artwork.name}"
